0e0a08c590a566cb1093d8983b93379534635c16
Guillem Hernandez Sola <guillemhernandezsola@Guillems-Mac-mini-M1.local>
2023-11-24 16:11:06 +0100

    First commit

---
+++ app/src/main/java/cat/fansubs/ott/webtoapp/MainActivity.java
@@ -0,0 +1,43 @@
+package cat.fansubs.ott.webtoapp;
+/*====================================
+    Author : Guillem Hernandez Sola
+======================================*/
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import cat.fansubs.ott.webtoapp.R;
+
+
+public class MainActivity extends Activity {
+    private WebView mywebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//for full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        mywebView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebView.loadUrl("https://www.fansubs.cat");
+        mywebView.setWebViewClient(new WebViewClient());//prevents our url to open in another browser
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mywebView.canGoBack()) {
+            mywebView.goBack();
+        } else {
+            super.onBackPressed();
+
+        }
+
+    }
+}
